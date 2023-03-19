36d9c5e7dbd2f2c450c743f7c7a0b549789ae800
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-03-19 07:29:17 +0100

    3/19/2023

---
+++ 7-kyu/ListOfpresents.js
@@ -0,0 +1,31 @@
+/**         Description
+ * Leo's girlfriend asked him to buy a gift list during his next trip, now he wants to know how many of them will he be able to buy.
+
+Write a function to help Leo out. The first parameter of the function is Leo's budget; the second one is an array containing the price of each gift. You should return an integer representing the maximum amount of gifts Leo can buy.
+
+Example:
+Maximum budget: 20
+Gift List: [13, 2, 4, 6, 1]
+Should return 4.
+
+_ NOTE: All numbers will be integers >= 0, and the array will never be empty. _
+ */
+
+
+//          Solution
+
+
+function howManyGifts(maxBudget, gifts) {
+    if(gifts.reduce((acc,c)=> acc + c ) <= maxBudget) {
+      return gifts.length
+      
+    }
+    let res = 0
+    gifts.sort((a,b)=> a-b)
+    while (maxBudget >= 0 && gifts.length > 0) {
+      maxBudget -= gifts[0]
+      gifts.shift()
+      res++
+    }
+    return res-1
+  }
