af965584f7bd3ea182ab3b182406fb4895504e4a
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-05-16 20:53:05 +0200

    5/16/2023

---
+++ 7-kyu/MostLikely.js
@@ -0,0 +1,23 @@
+/**
+ * Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+So:
+
+Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
+ */
+
+
+
+// solution
+
+function mostLikely(prob1,prob2){
+    let p1 =prob1.split(":")
+    p1 = p1[0] / p1[1]
+      
+    let p2 =prob2.split(":")
+    p2 = p2[0] / p2[1]
+      
+      return p1 > p2 
+    }
